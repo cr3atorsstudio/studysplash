8d8043ecafef38d5b4aa052195815bb75fd2e132
riho.takagi <rihohearts@gmail.com>
2023-09-23 12:35:25 -0400

    Create group

--- frontend/src/pages/mint/[role]/group/index.tsx
+++ frontend/src/pages/mint/[role]/group/index.tsx
@@ -1,5 +1,5 @@
 import { NextPageWithLayout } from "../../../_app";
-import { ReactElement, useCallback, useState } from "react";
+import { ReactElement, useCallback, useEffect, useState } from "react";
 import Layout from "@/layout";
 import Registration from "@/components/Registration";
 import {
@@ -18,10 +18,19 @@ import { useRouter } from "next/router";
 import CustomRadio from "@/components/CustomRadio";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { globalStore } from "@/store/global";
+import {
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { ERC1155_ABI } from "@/config/erc1155ABI";
 
 const Group: NextPageWithLayout = () => {
   const router = useRouter();
   const [groupName, setGroupName] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [nextTokenId, setNextTokenId] = useState(0);
   const setIsLoading = useSetRecoilState(globalStore.isLoading);
   const isLoading = useRecoilValue(globalStore.isLoading);
   const role = router.query.role;
@@ -30,14 +39,17 @@ const Group: NextPageWithLayout = () => {
   const toast = useToast();
 
   const avatars = [
-    { name: "group1", image: "🚀" },
-    { name: "group2", image: "🍔" },
-    { name: "group3", image: "☘️" },
-    { name: "group4", image: "🐼" },
-    { name: "group5", image: "🔮" },
+    { name: "1", image: "🚀" },
+    { name: "2", image: "🍔" },
+    { name: "3", image: "☘️" },
+    { name: "4", image: "🐼" },
+    { name: "5", image: "🔮" },
   ];
 
   const handleChange = (value: any) => {
+    setImageUrl(
+      `https://studysplash.s3.us-east-1.amazonaws.com/assets/emoji/${value}.svg`
+    );
     toast({
       title: `The image changed to ${value}!`,
       status: "success",
@@ -45,19 +57,77 @@ const Group: NextPageWithLayout = () => {
     });
   };
 
+  const { config, error } = usePrepareContractWrite({
+    address: "cre",
+    abi: ERC1155_ABI,
+    functionName: "mint",
+    chainId: 11155111,
+    args: ["0xE7FC4e1a7F980D30d4e408E3d3C0166B8bc68B30", nextTokenId],
+  });
+  const {
+    data: writeData,
+    isError: isWriteError,
+    isLoading: isContractWriteLoading,
+    write,
+  } = useContractWrite(config);
+  console.log("error", error);
+
+  useEffect(() => {
+    const unixTime = Math.floor(Date.now() / 1000);
+    setNextTokenId(unixTime);
+  }, []);
+
   const onMint = useCallback(() => {
     setIsLoading(true);
     // mintNFTを呼び出す
 
+    const json = {
+      name: groupName,
+      description:
+        "StudySplash🌊📚 is the cutting-edge cohort-based learning platform in the web3 space. Leveraging ERC6551, it records user interactions and course participations on the blockchain, fostering vibrant communication within cohorts and maintaining consistent learning motivation.",
+      image: imageUrl,
+    };
+    uploadGroupToS3(json);
     console.log("mintNFT");
-
-    setIsLoading(false);
-
-    router.push("/dashboard");
+    write?.();
   }, []);
+  const { isLoading: isWaitContractLoading, isSuccess: isWaitContractSuccess } =
+    useWaitForTransaction({
+      hash: writeData?.hash,
+    });
+
+  useEffect(() => {
+    //TODO: 失敗したとき
+    if (isWaitContractSuccess) {
+      toast({
+        title: `NFT minted successfully!`,
+        status: "success",
+        duration: 2000,
+      });
+      setIsLoading(false);
+
+      router.push("/dashboard");
+    }
+  }, [isWaitContractSuccess]);
+
+  const uploadGroupToS3 = async (jsonData: any) => {
+    const response = await fetch("/api/uploadgroup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: nextTokenId, jsonContent: jsonData }),
+    });
+    const result = await response.json();
+    if (response.ok) {
+      console.log("Uploaded successfully:", result.url);
+    } else {
+      console.error("Error uploading:", result.error);
+    }
+  };
 
   const { value, getRadioProps } = useRadioGroup({
-    defaultValue: "group1",
+    defaultValue: "1",
     onChange: handleChange,
   });
 
