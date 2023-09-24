a6d01008fee5818b5943c6717174cf739ecbc0b8
pluto00004 <ogura_miho@cyberagent.co.jp>
2023-09-24 00:36:34 -0400

    Add youtube

--- frontend/src/pages/study/index.tsx
+++ frontend/src/pages/study/index.tsx
@@ -1,9 +1,15 @@
 import DashboardLayout from "@/layout/dashboard";
 import { NextPageWithLayout } from "../_app";
 import { ReactElement } from "react";
+import Youtube from "react-youtube";
+import { Center } from "@chakra-ui/react";
 
 const Study: NextPageWithLayout = () => {
-  return <>video</>;
+  return (
+    <Center mt={10}>
+      <Youtube videoId="UDQlDF4TWYM" />
+    </Center>
+  );
 };
 
 Study.getLayout = function getLayout(page: ReactElement) {
